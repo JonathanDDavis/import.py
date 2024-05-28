289bb11d3e87c1f4400c27f50cf685b23a4169a0
JonathanDDavis <125523206+JonathanDDavis@users.noreply.github.com>
2024-05-28 14:00:57 -0500

    Add files via upload

---
+++ import.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      jd0919889
+#
+# Created:     22/10/2019
+# Copyright:   (c) jd0919889 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import globalFunctions
+import Child_Parent_ObjectEX
+
+globalFunctions.greeting("World")
+globalFunctions.printScore("over 9,000")
+globalFunctions.helloWorld()
+y = Child_Parent_ObjectEX.Student("Sally", "Student", "123-45-6789")
